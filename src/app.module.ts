47015d0c0063ed0b5a9636074015a12bfbe4cb07
Rendi <rendicrew96@gmail.com>
2024-09-14 03:02:52 +0700

    fix: core module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,30 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { DbModule } from './db/db.module';
+import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TodoModule } from './todo/todo.module';
+import { RedisModule } from '@nestjs-modules/ioredis';
 
 @Module({
-  imports: [],
+  imports: [
+    DbModule,
+    UserModule,
+    AuthModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TodoModule,
+    RedisModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: () => ({
+        type: 'single',
+        url: process.env.REDIS_URL,
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
